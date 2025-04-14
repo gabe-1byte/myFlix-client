703c0b40bad3f4d14b289ce6648a51bfded73e00
gabe-1byte <gabe.m.buckman@gmail.com>
2025-04-14 13:33:27 -0600

    CF 3.4: React Lifecycle Methods
    updates from CF 3.4: React Lifecycle Methods

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,25 +2,28 @@ export const MovieView = ({ movie, onBackClick }) => {
     return (
         <div>
             <div>
-                <img src={movie.image} />
+                <p>
+                    <strong>Image :</strong>
+                </p>
+                <img src={movie.Image} />
             </div>
             <div>
                 <p>
-                    <strong>Title:</strong> {movie.title}
+                    <strong>Title:</strong> {movie.Title}
                 </p>
             </div>
             <div>
                 <strong>Description: </strong>
-                <span style={{ whiteSpace: "pre-line"}}>{movie.description}</span>
+                <span style={{ whiteSpace: "pre-line"}}>{movie.Description}</span>
             </div>
             <div>
             <p>
-                <strong>Genre:</strong> {movie.genre}
+                <strong>Genre:</strong> {movie.Genre.Name}
             </p>
             </div>
             <div>
                 <p>
-                    <strong>Director:</strong> {movie.director}
+                    <strong>Director:</strong> {movie.Director.Name}
                 </p>
             </div>
             <button onClick={onBackClick}>Back</button>
